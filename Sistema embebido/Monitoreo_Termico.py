086e43b1b93b4ae140b6a3570fae1e322292a29f
daap213 <daap21.3@gmail.com>
2023-01-19 01:33:32 -0500

    Inicio automatico, Configuración temperatura

--- Sistema embebido/Monitoreo_Termico.py
+++ Sistema embebido/Monitoreo_Termico.py
@@ -8,22 +8,33 @@ import time
 import Bot_telegram
 import pi_therm_cam
 
-TempMax = 80
 inicio = time.time()
 fecha = time.ctime()
 fecha = fecha.split()
 dia = fecha[0]+"_"+fecha[1]+"_"+fecha[2]+"_"+fecha[4]
-path = os.getcwd()
+#path = os.getcwd()
+path = "/home/piDrone/Desktop/App"
 Datos = path+'/'+dia
-clasificador = cv2.CascadeClassifier('cascade.xml')
+clasificador = cv2.CascadeClassifier('/home/piDrone/Desktop/App/cascade.xml')
 cantidad = 0
-captura = cv2.VideoCapture(0) 
-Imagen_ref = None
- 
-def tempMax_chance(valor):
-    global TempMax
-    TempMax == int(valor)
-    visualizarVideo()
+time.sleep(3)
+captura = cv2.VideoCapture(0)
+time.sleep(3)
+Imagen_ref = None 
+window_width = 640
+window_height = 420
+root = None
+seleccionado = None
+lblVideo = None
+boton = None
+boton2 = None
+btnRadio1 = None
+btnRadio2 = None
+fondo = None
+image = None
+
+def tempMax_chance():
+    Ventana_video()
 
 def referencia():
     A = time.ctime()
@@ -76,6 +87,10 @@ def reconocimientoObj(ventana):
 
 def monitoreoAuto():
     global captura
+    global boton
+    global boton2
+    global btnRadio1
+    global btnRadio2
     btnRadio1.configure(state="disabled")
     boton.configure(state="active")
     boton2.configure(state="disabled")
@@ -86,12 +101,25 @@ def VideoTermo():
     #global captura
     #captura.release()
     #cv2.destroyAllWindows()
+    global boton
+    global boton2
+    global btnRadio1
+    global btnRadio2
+    
     boton.configure(state="active")
     boton2.configure(state="active")
     btnRadio1.configure(state="active")
     btnRadio2.configure(state="disabled")
     
 def verResultado():
+    global fondo
+    global seleccionado
+    global lblVideo
+    global boton
+    global boton2
+    global btnRadio1
+    global btnRadio2
+    
     seleccionado.set(0)
     lblVideo.configure(image=fondo)
     lblVideo.image = fondo
@@ -108,6 +136,8 @@ def  Enviar_telegram():
     global Datos
     global TempMax
     global captura
+    global root
+    global image
     Bot_telegram.stiker_telegram("CAACAgEAAxkBAAEHExhjrwgfvrsgKcUruOs42gVuVYzJYwACzgEAAqLNVzOBFRaZmhALYS0E",True,5) 
     Bot_telegram.mensaje_telegram("Fecha: "+dia,True,5)
     Bot_telegram.mensaje_telegram("Temperatura de reporte: "+str(TempMax)+" ºC",True,5)
@@ -144,7 +174,9 @@ def visualizarVideo():
     global captura
     global Imagen_ref
     global inicio
-
+    global seleccionado
+    global lblVideo
+    global fondo
     actual = time.time()
     if (actual-inicio)<=900:
         ret, ventana = captura.read()
@@ -156,7 +188,7 @@ def visualizarVideo():
             img = ImageTk.PhotoImage(image=im)
 
         if seleccionado.get() == 2 :
-            thermcam = pi_therm_cam.pithermalcam(output_folder = "")  # Instantiate class
+            thermcam = pi_therm_cam.pithermalcam(output_folder = "") 
             captu = thermcam.get_current_image_frame()
             ventana = cv2.resize(captu, (640, 380))
             Imagen_ref = ventana
@@ -169,48 +201,124 @@ def visualizarVideo():
     else:
         verResultado()
 
+def Ventana_video():
+    global root
+    global seleccionado
+    global lblVideo
+    global boton
+    global boton2
+    global btnRadio1
+    global btnRadio2
+    global fondo
+    global image
+    root = tk.Tk()
+    root.title("Monitoreo Térmico")
+    
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    center_x = int(screen_width/2 - window_width / 2)
+    center_y = int(screen_height/2 - window_height / 2)
+    root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-root = tk.Tk()
-root.title("Monitoreo Térmico")
-window_width = 640
-window_height = 420
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-center_x = int(screen_width/2 - window_width / 2)
-center_y = int(screen_height/2 - window_height / 2)
-root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+    tamaño = fnt.Font(size = 10)
+    seleccionado = tk.IntVar()
 
-tamaño = fnt.Font(size = 10)
-seleccionado = tk.IntVar()
+    lblVideo = tk.Label(root)
+    lblVideo.grid(column=0, row=0, columnspan=2,rowspan=40,sticky ="NWES")
 
-lblVideo = tk.Label(root)
-lblVideo.grid(column=0, row=0, columnspan=2,rowspan=40,sticky ="NWES")
+    boton = tk.Button(root, text="Ver resultados",bd = '3',font = tamaño, state="active", command=verResultado)
+    boton.grid(column=1, row=41, pady=1, sticky ="NWES")
 
-boton = tk.Button(root, text="Ver resultados",bd = '3',font = tamaño, state="active", command=verResultado)
-boton.grid(column=1, row=41, pady=1, sticky ="NWES")
+    boton2 = tk.Button(root, text="Tomar captura",bd = '3',font = tamaño, state="disabled", command=capturar)
+    boton2.grid(column=0,row=41, pady=1,sticky ="NWES")
 
-boton2 = tk.Button(root, text="Tomar captura",bd = '3',font = tamaño, state="disabled", command=capturar)
-boton2.grid(column=0,row=41, pady=1,sticky ="NWES")
+    btnRadio1 = tk.Radiobutton(root, text="Automático", variable=seleccionado,
+                            bd = '3',value = 1, indicator = 0,
+                            font = tamaño, state="active", command=monitoreoAuto)
+    btnRadio1.grid(column=0, row=0,sticky ="NWE")
 
-btnRadio1 = tk.Radiobutton(root, text="Automático", variable=seleccionado,
-                        bd = '3',value = 1, indicator = 0,
-                        font = tamaño, state="active", command=monitoreoAuto)
-btnRadio1.grid(column=0, row=0,sticky ="NWE")
+    btnRadio2 = tk.Radiobutton(root, text="Manual", variable=seleccionado,
+                            bd = '3',value = 2, indicator = 0,
+                            font = tamaño, state="active", command=VideoTermo)
+    btnRadio2.grid(column=1, row=0,sticky ="NWE")
+    image = cv2.imread(path+"/telegram.png")
+    image = cv2.resize(image, (640, 380)) 
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    fondo = ImageTk.PhotoImage(image=Image.fromarray(image))
+    btnRadio1.invoke()
+    visualizarVideo()
+    
 
-btnRadio2 = tk.Radiobutton(root, text="Manual", variable=seleccionado,
-                        bd = '3',value = 2, indicator = 0,
-                        font = tamaño, state="active", command=VideoTermo)
-btnRadio2.grid(column=1, row=0,sticky ="NWE")
+def on_button_click():
+    Princ.destroy()
 
-image = cv2.imread(path+"/telegram.png")
-image = cv2.resize(image, (640, 380)) 
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-fondo = ImageTk.PhotoImage(image=Image.fromarray(image))
+    global TempMax
+    global second_window
+    global label2
 
-image2 = cv2.imread(path+"/analisis.png")
-image2 = cv2.resize(image2, (640, 380)) 
-fondo2 = ImageTk.PhotoImage(image=Image.fromarray(image2))
+    second_window = tk.Tk()
+    second_window.title("Configuración de temperatura")
+    label2 = tk.Label(second_window, text="Temperatura de alerta: " + str(TempMax) +"ºC", font=("Arial", 18), justify=tk.LEFT)
+    label2.pack(expand=True, fill='both')
+    global scale
+    scale = tk.Scale(second_window, from_=10, to=300, orient=tk.HORIZONTAL, command=on_scale_change,length=290)
+    scale.set(TempMax)
+    scale.pack(side=tk.LEFT, padx=3, pady=3,expand=True)
+    button = tk.Button(second_window, text="Terminar", font=("Arial", 16), command=on_end_temp_click)
+    button.pack(side=tk.LEFT, padx=3, pady=3)
+    window_width = 400
+    window_height = 200
+    screen_width = second_window.winfo_screenwidth()
+    screen_height = second_window.winfo_screenheight()
+    center_x = int(screen_width/2 - window_width / 2)
+    center_y = int(screen_height/2 - window_height / 2)
+    second_window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+    second_window.configure(background='white')
 
-btnRadio1.invoke()
-visualizarVideo()
-root.mainloop()
+def on_end_temp_click():
+    second_window.destroy()
+    tempMax_chance()
+
+def on_close_click():
+    Princ.destroy()
+    tempMax_chance()
+
+def on_scale_change(val):
+    global TempMax
+    global label2
+    TempMax = int(val)
+    label2.config(text="Temperatura de alerta: " + str(TempMax) +"ºC")
+
+def center(toplevel):
+    toplevel.update_idletasks()
+    w = toplevel.winfo_screenwidth()
+    h = toplevel.winfo_screenheight()
+    size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
+    x = w/2 - size[0]/2
+    y = h/2 - size[1]/2
+    toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
+
+TempMax = 80
+Princ = tk.Tk()
+Princ.title("App Monitoreo")
+photo=tk.PhotoImage(file=path+"/mi_drone.PNG")
+
+canvas = tk.Canvas(Princ)
+canvas.create_image(230,130, image = photo, anchor = "center")
+canvas.create_text(245,250, fill = "Black",text="La temperatura de reporte es 80ºC\nPara cambiarla presione continuar\ncaso contrario espere 15s", font=("Arial", 17), anchor = "center")
+canvas.pack(expand=True, fill='both')
+
+windowWidth = 500
+windowHeight = 360
+screenWidth = Princ.winfo_screenwidth()
+screenHeight = Princ.winfo_screenheight()
+centerx = int(screenWidth/2 - windowWidth / 2)
+centery = int(screenHeight/2 - windowHeight / 2)
+Princ.geometry(f'{windowWidth}x{windowHeight}+{centerx}+{centery}')
+
+var = tk.IntVar()
+var.set(TempMax)
+button = tk.Button(Princ, text="Continuar?", font=("Arial", 16), command=on_button_click)
+button.pack(side=tk.BOTTOM, pady=2)
+Princ.after(15000, on_close_click)
+Princ.mainloop()
