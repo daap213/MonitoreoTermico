f19687443cb251a216209a9319097a83c6b932b9
daap213 <daap21.3@gmail.com>
2023-01-18 20:33:04 -0500

    Inicio APP

--- Sistema embebido/AppMonitoreo.py
+++ Sistema embebido/AppMonitoreo.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+import os
 
+path = os.getcwd()
 def on_button_click():
     root.destroy()
 
@@ -9,14 +11,14 @@ def on_button_click():
 
     second_window = tk.Tk()
     second_window.title("Configuración de temperatura")
-    label2 = tk.Label(second_window, text="Modifique el número: " + str(num), font=("Arial", 18), justify=tk.LEFT)
+    label2 = tk.Label(second_window, text="Temperatura de alerta: " + str(num) +"ºC", font=("Arial", 18), justify=tk.LEFT)
     label2.pack(expand=True, fill='both')
     global scale
-    scale = tk.Scale(second_window, from_=10, to=300, orient=tk.HORIZONTAL, command=on_scale_change)
+    scale = tk.Scale(second_window, from_=10, to=300, orient=tk.HORIZONTAL, command=on_scale_change,length=290)
     scale.set(num)
-    scale.pack(side=tk.LEFT, padx=10, pady=10)
-    button = tk.Button(second_window, text="Terminar", font=("Arial", 20), command=on_end_temp_click)
-    button.pack(side=tk.RIGHT, padx=10, pady=10)
+    scale.pack(side=tk.LEFT, padx=3, pady=3,expand=True)
+    button = tk.Button(second_window, text="Terminar", font=("Arial", 16), command=on_end_temp_click)
+    button.pack(side=tk.LEFT, padx=3, pady=3)
     window_width = 400
     window_height = 200
     screen_width = second_window.winfo_screenwidth()
@@ -28,21 +30,21 @@ def on_button_click():
 
 def on_end_temp_click():
     global num
-    #import Monitoreo_Termico
-    #Monitoreo_Termico.tempMax_chance(num)
+    import Monitoreo_Termico
+    Monitoreo_Termico.tempMax_chance(num)
     second_window.destroy()
 
 def on_close_click():
     global num
-    #import Monitoreo_Termico
-    #Monitoreo_Termico.tempMax_chance(num)
+    import Monitoreo_Termico
+    Monitoreo_Termico.tempMax_chance(num)
     root.destroy()
 
 def on_scale_change(val):
     global num
     global label2
     num = int(val)
-    label2.config(text="Modifique el número: " + str(num))
+    label2.config(text="Temperatura de alerta: " + str(num) +"ºC")
 
 def center(toplevel):
     toplevel.update_idletasks()
@@ -56,21 +58,24 @@ def center(toplevel):
 num = 80
 root = tk.Tk()
 root.title("App Monitoreo")
-label = tk.Label(root, text="La temperatura de reporte es 80ºC\nSi desea cambiarla presione continuar\ncaso contrario espere 15s", font=("Arial", 18), justify=tk.LEFT)
-label.pack(expand=True, fill='both',pady=10)
+photo=tk.PhotoImage(file=path+"/mi_drone.png")
+
+canvas = tk.Canvas(root)
+canvas.create_image(230,130, image = photo, anchor = "center")
+canvas.create_text(245,250, fill = "brown",text="La temperatura de reporte es 80ºC\nPara cambiarla presione continuar\ncaso contrario espere 15s", font=("Arial", 17), anchor = "center")
+canvas.pack(expand=True, fill='both')
 
 window_width = 500
-window_height = 250
+window_height = 360
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 center_x = int(screen_width/2 - window_width / 2)
 center_y = int(screen_height/2 - window_height / 2)
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-root.configure(background='white')
 
 var = tk.IntVar()
 var.set(num)
 button = tk.Button(root, text="Continuar?", font=("Arial", 16), command=on_button_click)
-button.pack(side=tk.BOTTOM, pady=10)
-root.after(10000, on_close_click)
+button.pack(side=tk.BOTTOM, pady=2)
+root.after(15000, on_close_click)
 root.mainloop()
